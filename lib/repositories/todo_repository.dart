22f63065b2cb5c31e69d856e4f78bf9c699269bf
unknown <malufreirejau@yahoo.com.br>
2022-06-06 12:40:43 -0300

    adding shared_preferences

---
+++ lib/repositories/todo_repository.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TodoRepository {
+
+  TodoRepository() {
+    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
+  }
+
+  late SharedPreferences sharedPreferences;
+
+  void exemplo() {
+    sharedPreferences.setString('nome', 'Malu');
+    sharedPreferences.getString('nome');
+  }
+}

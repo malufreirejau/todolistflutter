d70fec54ea4b1582248b8d93e020cc03c1a732c2
unknown <malufreirejau@yahoo.com.br>
2022-06-07 11:41:30 -0300

    loading todo_list

--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -4,16 +4,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/todo.dart';
 
+const todoListKey = 'todo_list';
+
 class TodoRepository {
+
   TodoRepository() {
-  SharedPreferences.getInstance()
-    .then((value) => {sharedPreferences = value});
-  }
 
   late SharedPreferences sharedPreferences;
 
+  Future<List<Todo>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final List jsonDecoded = json.decode(jsonString) as List;
+    return jsonDecoded.map((e) => Todo.fromJson(e)).toList();
+  }
+
   void saveTodoList(List<Todo> todos) {
     final String jsonString = json.encode(todos);
-    sharedPreferences.setString('todo_list', jsonString);
+    sharedPreferences.setString(todoListKey, jsonString);
   }
 }
